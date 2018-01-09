153519b77036c2bb9d182c9423c3ce5130e21c78
shadowzack <battleforge-war@hotmail.com>
2018-01-09 19:37:50 +0200

    12

--- inc/init.php
+++ inc/init.php
@@ -1,12 +1,9 @@
 <?php
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', '1');
 
 $_GET['module']=isset($_GET['module']) ? $_GET['module']:"engineer";
 $_GET['page']=isset($_GET['page']) ? $_GET['page']:"list";
 $FILE_TO_LOAD='inc/modules/'.$_GET['module'].'/'.$_GET['page'].'.php';
-//echo $FILE_TO_LOAD;
 include('config.php');
 
 
