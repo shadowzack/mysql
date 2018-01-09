bd611f9437f9dbca6b4506dd0d169bc9272ab01f
shadowzack <battleforge-war@hotmail.com>
2018-01-10 00:28:50 +0200

    13

--- inc/config.php
+++ inc/config.php
@@ -135,5 +135,164 @@ echo "Error creating table: " . mysqli_error($conn)."<br>";
 }
 
 
+$sql="INSERT INTO `development_stages` (`project_id`, `tests`, `design`, `con_management`, `requirements`, `coding`) VALUES
+(1, 'visual', 'mvc', 'git', 'server side', 'c'),
+(2, 'vsiual', 'oop', 'git', 'server clinet', 'php mysl'),
+(3, 'code blocks', 'oop', 'git bash', 'linux', 'c,c++'),
+(4, 'vs code', 'mvc', 'srver echo', 'linux', 'php'),
+(5, 'eclipse', 'play framework', 'git', 'scala', 'scala java'),
+(6, 'xcode', 'nfc', 'github', 'nfc compatible', 'swift java'),
+(7, 'street of curse', 'sensor api', 'git lib', 'sensor machine learing', 'any'),
+(8, 'atom', 'mvc', 'git hub', 'server clint', 'php mysql '),
+(9, 'visual', 'mvc', 'git lib phmyadmin', 'storage server', 'php mysql'),
+(10, 'no test ', 'unix bsd', 'command', 'unix knoledge', 'c');
+";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `engineer` (`id`, `firstname`, `lastname`, `addresss`, `age`, `birthdate`) VALUES
+(1, 'mahmod', 'hasan', 'haifa', 20, '06/19/1997'),
+(2, 'avi', 'ytsak', 'tel-aviv', 27, '12/12/1990'),
+(3, 'jolyana', 'bder', 'tira', 24, '12/11/1993'),
+(4, 'mechal', 'lorn', 'hdera', 31, '05/22/1986'),
+(5, 'omer', 'peli', 'haifa', 23, '01/24/1994'),
+(6, 'mohamd', 'amer', 'nazerah', 25, '03/06/1992'),
+(7, 'ytask', 'merkav', 'htzfon', 21, '06/06/1996'),
+(8, 'yoni', 'rbin', 'nharya', 18, '03/07/1999'),
+(9, 'ahmad', 'mhamid', 'kfar kra', 24, '10/22/1993'),
+(10, 'david', 'jack', 'hertslya', 29, '02/19/1988');
+";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `has` (`id`, `field_name`) VALUES
+(1, 'client side'),
+(2, 'client side'),
+(3, 'data base mangment'),
+(5, 'fullstack'),
+(7, 'meanstack'),
+(4, 'moblie'),
+(8, 'moblie'),
+(6, 'server side'),
+(9, 'unix'),
+(10, 'unix kernel');";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `milestones` (`project_id`, `product_name`, `budget`, `datee`) VALUES
+(1, 'pp', 200000, '12/12/1999'),
+(2, 'produ', 5220, '04/05/2001'),
+(3, 'straming', 5515521, '12/12/1997'),
+(4, 'ech srveer', 53456, '02/22/2005'),
+(5, 'java', 252515, '01/04/1988'),
+(6, 'wirless', 56454, '04/06/2007'),
+(7, 'auto driveing', 4531, '12/08/2013'),
+(8, 'social', 521111, '05/07/2009'),
+(9, 'drive onlie ', 400000, '12/09/2001'),
+(10, 'x linux', 1, '01/15/2018');";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `phone` (`phone`, `id`) VALUES
+(75867857, 1),
+(4294967295, 1),
+(665546, 2),
+(5478567, 2),
+(4526456, 4),
+(47475, 5),
+(5464785, 6),
+(4757486, 7),
+(424536465, 8),
+(7578, 10),
+(7865787, 10),
+(475647567, 10);";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `projects` (`project_id`, `project_name`, `starting_time`, `customer_name`, `taoor`) VALUES
+(1, 'web os', '11/11/2000', 'yoni', 'web opreation system'),
+(2, 'virtual one', '12/05/2005', 'noamn', 'windows virtual os'),
+(3, 'x stramer', '03/12/2007', 'amer', 'online stramer for web aplications'),
+(4, 'draw 3d', '11/05/2001', 'mohamd', 'drawing 3d objects'),
+(5, 'controlless', '12/22/2004', 'tmir', 'conrtol planes wirlessly'),
+(6, 'charge free', '02/22/1999', 'moamin', 'wirless charging'),
+(7, 'hands free', '05/06/2009', 'omar', 'autonomis driving'),
+(8, 'live', '05/05/2013', 'mahmod', 'social media with every thing live'),
+(9, 'mega drive', '06/06/1999', 'keli', 'cloud storage sulition'),
+(10, 'linux x', '08/08/2018', 'mahmod hasan', 'Linux destro our final project');
+";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+$sql="INSERT INTO `software_field` (`id`, `field_name`, `expertise`) VALUES
+(1, 'web', 'low'),
+(2, 'unix', 'low'),
+(3, 'fullstack', 'hight'),
+(4, 'meanstack', 'low'),
+(5, 'unix kernel', 'very hieght'),
+(6, 'server side', 'low'),
+(7, 'client side', 'hight'),
+(8, 'windows server', 'maduim'),
+(9, 'moblie', 'student'),
+(10, 'data base mangment', 'height');";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
+
+
+$sql="INSERT INTO `works` (`project_id`, `engineer_id`, `grade`) VALUES
+(1, 1, 7),
+(1, 2, 1),
+(1, 5, 4),
+(2, 1, 5),
+(2, 4, 2),
+(2, 10, 5),
+(3, 1, 3),
+(3, 4, 1),
+(3, 8, 5),
+(3, 9, 5),
+(3, 10, 8),
+(4, 1, 1),
+(4, 5, 5),
+(4, 7, 5),
+(5, 1, 3),
+(5, 7, 5),
+(6, 1, 5),
+(6, 4, 2),
+(7, 1, 5),
+(7, 3, 5),
+(8, 1, 6),
+(8, 3, 4),
+(8, 5, 4),
+(8, 6, 4),
+(8, 8, 7),
+(8, 9, 8),
+(9, 1, 5),
+(10, 1, 6);";
+$query=mysqli_query($conn,$sql);
+if(!$query)
+{
+echo "Error creating table: " . mysqli_error($conn)."<br>";
+}
 
 ?>
--- inc/modules/engineer/addproject.php
+++ inc/modules/engineer/addproject.php
@@ -73,3 +73,119 @@ die;
     </form>
   </div>
 </div>
+
+
+<?php 
+
+/*
+   $tmpid=$_GET['id'] ;
+                   $sql="SELECT  projects.project_name,works.engineer_id,projects.project_id
+                   FROM projects
+                   INNER JOIN works
+                   ON projects.project_id=works.project_id
+                   WHERE  engineer_id!=$tmpid";
+                   $sql2="SELECT project_name,project_id
+                   FROM projects
+                  ";
+                   $sql="SELECT DISTINCT  T.project_name,T.pid,engineer_id FROM(SELECT project_name ,project_id AS pid
+                   FROM projects) AS T
+                   INNER JOIN works
+                   ON T.pid=works.project_id
+                   WHERE works.engineer_id=$tmpid
+                    ";
+                  $res=mysqli_query($conn,$sql);
+                
+                  $res2=mysqli_query($conn,$sql2);
+
+                  $count2=mysqli_num_rows($res);
+                  echo($count2);
+                  if ($res) {
+                    $myarr=[];
+                    $i=0;
+                
+                    while($row=mysqli_fetch_assoc($res))
+                    {
+                      $myarr[$i]=$row['project_name'];
+                      $i++;
+                      echo $row['project_name'];
+                    }
+                    $mainarr=[];
+                    $mainarrkey=[];
+                    $f=0;
+                    
+                    while($row=mysqli_fetch_assoc($res2))
+                    {
+                     // foreach($row as $key => $var)
+ // {
+  //echo $row['project_name'];
+    //  echo $key . ' = ' . $var . '<br />';
+    
+   for ($z=0; $z < $i; $z++) { 
+    
+     if (!array_search($row['project_name'],$myarr)) {
+     
+     if (!array_search($row['project_name'],$mainarr)) {
+       $mainarr[$f]=$row['project_name'];
+       $mainarrkey=[$f]=$row['project_id'];
+       $f++;
+     }
+     }
+   }
+
+  //}
+}
+for ($l=0; $l < $f; $l++) { 
+ echo $mainarr[$l];
+ echo $mainarrkey[$l];
+ echo "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ $c=0;
+                              $sqll="SELECT projects.project_name,works.engineer_id,projects.project_id,works.grade
+                              FROM projects
+                              INNER JOIN works
+                              ON projects.project_id=works.project_id
+                              WHERE engineer_id=$tmpid ";
+                             $res2=mysqli_query($conn,$sqll);
+                  while($row=mysqli_fetch_assoc($res))
+                  {
+                        while ($row2=mysqli_fetch_assoc($res2)) {
+                          if ($row2['project_id']!=$row['project_id']) {
+                            $c++;
+                          }
+                        }
+                        mysqli_data_seek($res2, 0);
+                        if ($c>0) {
+                          $c=0;
+                        
+                      ?>
+                         <option value="<?=$row['project_id'];?>">
+                                    <?=$row['project_name'];?>
+                                </option>
+                 <?php
+                        }
+                  }
+                
+*/
+?>
